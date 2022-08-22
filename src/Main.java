e43d577ea6a5bd2f77a703e8cb55c0d308cc9add
nadillustrator <nadin2208@gmail.com>
2022-08-23 01:24:02 +0300

    HW_SP3 additional tasks

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-       //Task 1
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+
+    public static void task1() {
         int clientOS = 1;
-        if(clientOS == 0){
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }else{
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println();
+    }
 
 
-        //Task 2
+    public static void task2() {
+        int clientOS = 1;
         int clientDeviceYear = 2017;
-        if(clientDeviceYear < 2015 && clientOS == 0){
+        if (clientDeviceYear < 2015 && clientOS == 0) {
             clientOS = 2;
-        }else if(clientDeviceYear < 2015 && clientOS == 1){
+        } else if (clientDeviceYear < 2015 && clientOS == 1) {
             clientOS = 3;
         }
-        switch (clientOS){
+        switch (clientOS) {
             case 0:
                 System.out.println("Установите версию приложения для iOS по ссылке");
                 break;
@@ -32,95 +44,84 @@ public class Main {
                 break;
         }
         System.out.println();
+    }
 
 
-        //Task 3
+    public static void task3() {
         int year = 1992;
-        if(year%4 == 0 && year%100 == 0 && year%400 == 0){
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
             System.out.println(year + " год является високосным");
-        }else if(year%4 == 0 && year%100 != 0) {
+        } else if (year % 4 == 0 && year % 100 != 0) {
             System.out.println(year + " год является високосным");
-        }else{
+        } else {
             System.out.println(year + " год не является високосным");
         }
         System.out.println();
+    }
 
 
-        //Task 4
+    public static void task4() {
         int deliveryDistance = 95;
         int dayCounter = 1;
-        if(deliveryDistance < 20){
-            System.out.println("Потребуется дней: " + dayCounter);
-        } else if(deliveryDistance >=20 && deliveryDistance < 60){
+
+        if (deliveryDistance >= 20) {
             dayCounter++;
-            System.out.println("Потребуется дней: " + dayCounter);
-        } else{
-            dayCounter += 2;
-            System.out.println("Потребуется дней: " + dayCounter);
         }
+        if (deliveryDistance >= 60) {
+            dayCounter++;
+        }
+
+        System.out.println("Потребуется дней: " + dayCounter);
         System.out.println();
+    }
 
 
-        //Task 5
-        int month = 8;
-        if(month > 0 || month < 13){
+    public static void task5() {
+        int monthNumber = 8;
 
-            switch (month) {
-                case 1:
-                System.out.println("January");
-                break;
+        switch (monthNumber) {
+            case 12:
+            case 1:
             case 2:
-                System.out.println("February");
+                System.out.println("Winter");
                 break;
             case 3:
-                System.out.println("March");
-                break;
             case 4:
-                System.out.println("April");
-                break;
             case 5:
-                System.out.println("May");
+                System.out.println("Spring");
                 break;
             case 6:
-                System.out.println("June");
-                break;
             case 7:
-                System.out.println("July");
-                break;
             case 8:
-                System.out.println("August");
+                System.out.println("Summer");
                 break;
             case 9:
-                System.out.println("September");
-                break;
             case 10:
-                System.out.println("October");
-                break;
             case 11:
-                System.out.println("November");
+                System.out.println("Autumn");
                 break;
-            case 12:
-                System.out.println("December");
-                break;
-            }
+            default:
+                System.out.println("Incorrect month");
         }
+
         System.out.println();
+    }
 
 
-        //Task 6 additional task
+    public static void task6() {
         int age = 19;
         int salary = 58_000;
         int creditLimit;
-        if(age > 18 && age < 23){
+        if (age > 18 && age < 23) {
             creditLimit = salary * 2;
-            if(salary >= 50_000){
+            if (salary >= 50_000) {
                 creditLimit *= 1.2;
             } else if (salary >= 80_000) {
                 creditLimit *= 1.5;
             }
-        }else{
+        } else {
             creditLimit = salary * 3;
-            if(salary >= 50_000){
+            if (salary >= 50_000) {
                 creditLimit *= 1.2;
             } else if (salary >= 80_000) {
                 creditLimit *= 1.5;
@@ -128,36 +129,37 @@ public class Main {
         }
         System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
         System.out.println();
+    }
 
 
-        //Task 7 additional task
-        age = 25;
-        salary = 60_000;
+    public static void task7() {
+        int age = 25;
+        int salary = 60_000;
         double interestRate = 1.1;
         int loanTerms = 12;
         double maximumMonthlyPayment = salary * 0.5;
         int wantedSum = 330_000;
         double monthlyPayment;
 
-        if(age < 23){
+        if (age < 23) {
             interestRate *= 1.01;
-        } else if(age < 30){
+        } else if (age < 30) {
             interestRate *= 1.005;
         }
 
-        if(salary > 80_000){
+        if (salary > 80_000) {
             interestRate = interestRate - (interestRate * 0.007);
         }
 
         monthlyPayment = wantedSum * interestRate / loanTerms;
-        if(maximumMonthlyPayment < monthlyPayment){
+        if (maximumMonthlyPayment < monthlyPayment) {
             System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maximumMonthlyPayment +
-                    " рублей. Платеж по кредиту "+ monthlyPayment +" рублей. Отказано");
-        } else{
+                    " рублей. Платеж по кредиту " + monthlyPayment + " рублей. Отказано");
+        } else {
             System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maximumMonthlyPayment +
-                    " рублей. Платеж по кредиту "+ monthlyPayment +" рублей. Одобрено");
+                    " рублей. Платеж по кредиту " + monthlyPayment + " рублей. Одобрено");
         }
-
-
     }
+
+
 }
